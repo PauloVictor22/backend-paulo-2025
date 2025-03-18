12d89d4d7d1094288b3ad9a85e0ca1d67e40e8e5
PauloMuhammad <160732116+PauloVictor22@users.noreply.github.com>
2025-03-18 11:55:25 +0000

    commit update e delete

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,16 +1,56 @@
+import { z } from "zod";
+
+const PaymentSchema = z.object({
+    data: z.string().datetime({ message: 'Data inválida' }),
+    recibo: z.number({ message: 'Recibo inválido' }).min(1, { message: 'Recibo inválido' }),
+    valor: z.number({ message: 'Valor inválido' }).min(0, { message: 'Valor inválido' }),
+    observacao: z.string().optional(),
+});
+
 const PaymentController = {
 
 
 
-    async createPayment(req,res) {
+    async createPayment(req, res) {
         try {
-            const {data, recibo, valor, observacao} = req.body;
-            return res.status(201).json({ message: 'Payment created', data: {data, recibo, valor, observacao} });
+            const { data, recibo, valor, observacao } = req.body;
+            PaymentSchema.parse({ data, recibo, valor, observacao });
+            return res.status(201).json({ message: 'Payment created', data: { data, recibo, valor, observacao } });
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({ message: "Validation error", details: error.error });
+            }
             return res.status(500).json({ error: 'Internal server error' });
         }
 
     },
+
+    async updatePayment(req, res) {
+        const { id } = req.params;
+        try {
+            const { data, recibo, valor, observacao } = req.body;
+            PaymentSchema.parse({ data, recibo, valor, observacao });
+            return res.status(200).json({
+                message: 'Payment updated',
+                data: { data, recibo, valor, observacao }
+            });
+
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({ message: "Validation error", details: error.error });
+            }
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    },
+
+    async deletePayment(req, res) {
+        try {
+            const { id } = req.params;
+            return res.status(200).json({ message: 'Payment deleted', id });
+        } catch (error) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
 
 };
 export default PaymentController;
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 const router = express.Router();
 
 import PaymentController from "../controllers/payment.controller.js";
-
-router.post("/payment", PaymentController.createPayment);
+router.patch("/payment/:id", PaymentController.updatePayment);
+router.post("/payment/:id", PaymentController.createPayment);
+router.delete("/payment/:id", PaymentController.deletePayment);
 
 export default router;
